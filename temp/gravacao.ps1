b7f260fc74a4277526cdf34264fa0eba658467f3
Sérgio Roberto Delfino <sergio.delfino@fatecourinhos.edu.br>
2025-06-02 18:57:58 +0000

    Atualizações

--- temp/gravacao.ps1
+++
@@ -1,9 +0,0 @@
-Clear-Host
-
-$data = "26/05/2025"
-$temperatura = 29.5
-
-$linha = $data + ";" + $temperatura
-
-$arquivo = "/workspaces/sistemas-operacionais-2025-01/temp/temperaturas.txt"
-Add-Content -Path $arquivo -Value $linha
